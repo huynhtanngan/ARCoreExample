f2ad53264b5e8a7a72d8e1d8092d32c247a04f38
nganht2 <nganht2@vng.com.vn>
2018-11-19 23:19:51 +0700

    add ar fragment

--- app/src/main/java/vn/nganht/arcoreexample/MainActivity.kt
+++ app/src/main/java/vn/nganht/arcoreexample/MainActivity.kt
@@ -1,12 +1,16 @@
 package vn.nganht.arcoreexample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.ar.sceneform.ux.ArFragment
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fragment: ArFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fragment = supportFragmentManager.findFragmentById(R.id.sceneformFragment) as ArFragment
     }
 }
